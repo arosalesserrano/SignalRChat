7da0ddf79548fa723f670e79ea314f5be165dfcb
arosalesserrano <50445878+arosalesserrano@users.noreply.github.com>
2019-05-29 16:26:38 +0200

    Cambios de prueba 3

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -7,7 +7,7 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
+    var encodedMsg = user + " puntua " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
@@ -20,7 +20,7 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    var user = "E1";//document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
