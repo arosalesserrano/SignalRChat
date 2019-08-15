f74504c70cb3936073cbc0befc8439bb197d77f2
arosalesserrano <50445878+arosalesserrano@users.noreply.github.com>
2019-08-15 13:38:41 +0200

    prueba de devolucion javascript con mensaje inicial

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -25,6 +25,9 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
+    connection.invoke("SendMessage", "arosalesii", "Inicio").catch(function (err) {
+        return console.error(err.toString());
+    });
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
