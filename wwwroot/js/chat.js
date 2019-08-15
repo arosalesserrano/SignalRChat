446adec16868b290d9d5c01efe844dd4fdf61cd5
arosalesserrano <50445878+arosalesserrano@users.noreply.github.com>
2019-08-15 13:59:04 +0200

    prueba devolucion json de cliente js 10

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,7 +1,7 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("http://192.168.0.94:5000/chatHub").build();
-var msgprueba;
+var message="";
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
@@ -10,7 +10,6 @@ connection.on("ReceiveMessage", function (user, message) {
     //var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     // segun el tipo de mensaje lo pondremos en la lista messagesList o en la lista logList
     //if(message.tipomensaje)
-    msgprueba = message; // variable que recoge la estructura json pa probar si la puedo devolver
     var msg = message.nota;
     var encodedMsg = user + " tipo mensaje: " + message.tipomensaje + ". Puntua " + msg;
     document.getElementById("dorsalInput").value = msg;
@@ -38,11 +37,9 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     //var puntuacion = document.getElementById("enteroInput").value + "." + document.getElementById("decimaInput").value;
     //var dorsal = document.getElementById("dorsalInput").value;
     //var message = user + "," + puntuacion + "," + dorsal;
-    var message;
-    msgprueba.tipomensaje = "Control";
-    msgprueba.nota = "Enviado desde javascript";
-    msgprueba.usuario = document.getElementById("userInput").value;
-    message = msgprueba;
+    message.tipomensaje = "Control";
+    message.nota = "Enviado desde javascript";
+    message.usuario = document.getElementById("userInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
